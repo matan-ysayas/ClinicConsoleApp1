791ef81c5950d420cf36f061dc1e2d6fed1d39fe
matan-ysayas <matan.ysayas@gmail.com>
2021-12-06 18:20:14 +0200

    Add project files.

---
+++ ClinicConsoleApp1/Doctor.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClinicConsoleApp1
+{
+    internal class Doctor: Employee,IComparable
+    {
+
+
+        public int numOfPatients;
+        public string yearOfBirth;
+
+       public Doctor(string firstName, string lastName, string job, int numOfPatients, string yearOfBirth ) : base(firstName, lastName, job)
+        {
+            this.numOfPatients = numOfPatients;
+            this.yearOfBirth = yearOfBirth; 
+        }
+
+ 
+        public int CompareTo(object obj)
+        {
+            Doctor d = (Doctor)obj;
+            if (this.numOfPatients > d.numOfPatients)
+                return -1;
+            if (this.numOfPatients < d.numOfPatients)
+                return 1;
+            return 0;
+        }
+
+    }
+
+    
+}
---
+++ ClinicConsoleApp1/Program.cs
@@ -0,0 +1,85 @@
+﻿using ClinicConsoleApp1;
+
+List<Doctor> doctorsList = new List<Doctor>();
+
+
+void addDoctor()
+{
+    Console.WriteLine("enter doctor first name");
+    string newDoctorName = Console.ReadLine();
+    Console.WriteLine("enter docrot last name");
+    string newDoctorLastName = Console.ReadLine();
+    Console.WriteLine("enter the doctor job");
+    string newDoctorJob = Console.ReadLine();
+    Console.WriteLine("enter number of patients");
+    int newDoctorNumOfPatients = int.Parse(Console.ReadLine());
+    Console.WriteLine("enter doctor B day");
+    string newDoctorBDay = Console.ReadLine();
+
+    Doctor doctorToAdd = new Doctor(newDoctorName, newDoctorLastName, newDoctorJob, newDoctorNumOfPatients, newDoctorBDay);
+    doctorsList.Add(doctorToAdd);
+  
+    createDoctorFile(doctorToAdd);
+
+    addToAllDoctorsFile(doctorsList);
+
+}
+
+
+void createDoctorFile(Doctor obj)
+{
+    FileStream fs = new FileStream($@"c:\test\{obj.firstName}.txt", FileMode.Append);
+    using (StreamWriter writer = new StreamWriter(fs))
+    {
+        writer.WriteLine($" first name:{obj.firstName} || last name:{obj.lastName} || job:{obj.job} || number of patients:{obj.numOfPatients} || B day:{obj.yearOfBirth}");
+    }
+
+
+
+}
+
+void addToAllDoctorsFile( List<Doctor> doctorList)
+{
+    FileStream fs = new FileStream($@"c:\test\allDoctors.txt", FileMode.Create);
+    using (StreamWriter writer = new StreamWriter(fs))
+    {
+        for(int i = 0; i < doctorList.Count; i++)
+        {
+            writer.WriteLine($"id:{i} first name:{doctorList[i].firstName} || last name:{doctorList[i].lastName} || job:{doctorList[i].job} || number of patients:{doctorList[i].numOfPatients} || B day:{doctorList[i].yearOfBirth} ,");
+
+        }
+
+    }
+
+
+}
+
+
+ 
+
+
+
+
+void ClinicApp()
+{
+
+    Console.WriteLine($"for add a doctor press 1 \n for get a doctor press2\n for add a Patient to a doctor press 3\n for get all the doctors press 4");
+
+    int options = int.Parse(Console.ReadLine());
+
+    switch (options)
+    {
+        case 1:
+            addDoctor();
+            ClinicApp();
+            break;
+
+
+
+
+
+    }
+
+};
+
+ClinicApp();
